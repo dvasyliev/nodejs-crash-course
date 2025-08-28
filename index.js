09854209d48e5abba2ee6c282e45120f2ce72b41
dvasyliev <dvasyliev.dev@gmail.com>
2025-08-28 17:58:53 +0200

    S04. L04. Configuring Environment Variables

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 let http = require("http");
+
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.NODE_ENV || "production";
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("Hello from a server!");
   })
-  .listen(8080);
+  .listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT} in ${ENV} mode`);
+  });
